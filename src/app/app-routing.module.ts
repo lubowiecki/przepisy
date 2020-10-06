3727b1534cd058d68d7a86721b3e09519a62f917
Dawid <lubowiecki@gmail.com>
2020-10-06 21:59:40 +0200

    remove redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+const routes: Routes = [ 
   {
     path: '',
-    redirectTo: 'przepisy',
-    pathMatch: 'full'
-  },
-  {
-    path: 'przepisy',
     loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule)
   }
 ];
