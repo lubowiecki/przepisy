ec57a08c9b985c9a111abbfc9efc72e89e5587cb
Dawid <lubowiecki@gmail.com>
2020-10-06 21:53:29 +0200

    redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [ 
+const routes: Routes = [
   {
     path: '',
+    redirectTo: 'przepisy',
+    pathMatch: 'full'
+  },
+  {
+    path: 'przepisy',
     loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule)
   }
 ];
